40cf4bb05da2df7b89dc4e6349700dd9c961299f
KazMalhotra <kazm31416@gmail.com>
2020-02-02 18:22:12 -0800

    Get rid of python stuff. See https://github.com/CGMSP/exercises

--- python/divisible?.py
+++
@@ -1,6 +0,0 @@
-def divisible(dividend, divisor): #Define function divisible with parameters dividend and divisor.
-  if dividend % divisor == 0: # If dividend fits into divisor with no remainder, Output is "True"
-    return True
-  else: #Otherwise, output is false
-    return False
-# This is not a complete program. For use, import this into a script that makes use of the function.
